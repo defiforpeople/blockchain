35cb004807741348263895f98d167a03870e5e13
NicoBarragan <nbarragan01@gmail.com>
2022-05-10 16:35:40 -0300

    refactor(aaveInteract.ts): set function params instead of hardcoding

--- src/scripts/aaveInteract.ts
+++ src/scripts/aaveInteract.ts
@@ -1,23 +1,25 @@
 import { ethers } from "hardhat";
 
-async function main() {
+export default async function main(
+  aavePoolAddress: string,
+  userAddress: string
+) {
   const TestingAavePool = await ethers.getContractFactory("TestingAavePool");
-  const aaveContract = await TestingAavePool.deploy(
-    "0xE039BdF1d874d27338e09B55CB09879Dedca52D8"
-  );
+  const aaveContract = await TestingAavePool.deploy(aavePoolAddress);
 
   await aaveContract.deployed();
 
   console.log("Address: ", aaveContract.address);
 
-  const userInfo = await aaveContract.getUser(
-    "0x57ac4E23aE911Cb3aEDAfE9ABb8E68a68F7CC463"
-  );
+  const userInfo = await aaveContract.getUser(userAddress);
 
   console.log("User info on aaveContract:", userInfo);
 }
 
-main().catch((error) => {
+main(
+  "0xE039BdF1d874d27338e09B55CB09879Dedca52D8",
+  "0x57ac4E23aE911Cb3aEDAfE9ABb8E68a68F7CC463"
+).catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
