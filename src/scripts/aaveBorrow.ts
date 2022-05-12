752de76d10760005ed9bc84310a58df5fff76cd5
NicoBarragan <nbarragan01@gmail.com>
2022-05-12 20:56:32 -0300

    refactor(aaveBorrow.ts): receives the amount as a param. changed the interest rate from 1 to 2

--- src/scripts/aaveBorrow.ts
+++ src/scripts/aaveBorrow.ts
@@ -1,11 +1,13 @@
 import { ethers } from "hardhat";
 import "@nomiclabs/hardhat-ethers";
 // eslint-disable-next-line camelcase
-import { IPool__factory, IPool } from "../typechain";
+import { IPool__factory, IPool, IERC20__factory, IERC20 } from "../typechain";
+import { BigNumber } from "ethers";
 
 export default async function main(
   aavePoolAddress: string,
-  tokenAddress: string
+  tokenAddress: string,
+  amount: BigNumber
 ) {
   const wallets = await ethers.getSigners();
   const wallet = wallets[0];
@@ -15,17 +17,18 @@ export default async function main(
     aavePoolAddress
   )) as IPool;
 
-  const max = 0.0113252;
-  console.log("max", ethers.utils.parseEther(`${max}`));
-  const amount = ethers.utils.parseEther(`${max / 2}`);
-  console.log("Amount: ", amount);
+  const token = (await ethers.getContractAt(
+    IERC20__factory.abi,
+    tokenAddress
+  )) as IERC20;
+
   const gasLimit = 2074040;
 
   console.log("Borrowing...");
   const supTx = await aavePool.borrow(
-    tokenAddress,
+    token.address,
     amount,
-    1, // 1: variable | 2: stable
+    2, // 1: stable | 2: variable
     0,
     wallet.address,
     {
@@ -37,9 +40,12 @@ export default async function main(
   console.log("TX: ", supTx);
 }
 
+const max = 0.0063795;
+const amount = ethers.utils.parseEther(`${max / 2}`);
+console.log("Amount: ", amount);
 const rinkebyWETH = "0xd74047010D77c5901df5b0f9ca518aED56C85e8D";
 // const rinkebyWBTC = "0x124F70a8a3246F177b0067F435f5691Ee4e467DD";
-main("0xE039BdF1d874d27338e09B55CB09879Dedca52D8", rinkebyWETH).catch(
+main("0xE039BdF1d874d27338e09B55CB09879Dedca52D8", rinkebyWETH, amount).catch(
   (error) => {
     console.error(error);
     process.exitCode = 1;
