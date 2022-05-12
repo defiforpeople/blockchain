8f0af16c0becbba72157b0a327ef306083c55170
NicoBarragan <nbarragan01@gmail.com>
2022-05-12 02:35:21 -0300

    feat(aaveSupply): WIP implemented approve and supply tx on aave, but fails because of the testnet tokens

--- src/scripts/aaveSupply.ts
+++ src/scripts/aaveSupply.ts
@@ -1,16 +1,59 @@
 import { ethers } from "hardhat";
-import { IPool } from "@aave";
+import "@nomiclabs/hardhat-ethers";
+// eslint-disable-next-line camelcase
+import { IPool__factory, IPool, IERC20, IERC20__factory } from "../typechain";
+import { Signer } from "ethers";
+
 export default async function main(
   aavePoolAddress: string,
-  userAddress: string
+  tokenAddress: string
 ) {
-  const aavePool = await ethers.getContractAt(IPool, aavePoolAddress);
+  const wallets = await ethers.getSigners();
+  const wallet = wallets[0];
+
+  const token = (await ethers.getContractAt(
+    IERC20__factory.abi,
+    tokenAddress
+  )) as IERC20;
+
+  const aavePool = (await ethers.getContractAt(
+    IPool__factory.abi,
+    aavePoolAddress
+  )) as IPool;
+
+  const amount = ethers.utils.parseEther("0.000003");
+  console.log("Amount: ", amount);
+  console.log("Approving...");
+  const gasLimit = 2074040;
+  const tx = await token.approve(aavePool.address, amount, {
+    from: wallet.address,
+    gasLimit: gasLimit,
+  });
+
+  await tx.wait();
+  console.log(tx);
+  console.log("Approved!");
+
+  console.log("Supplying...");
+  const supTx = await aavePool.supply(
+    token.address,
+    amount,
+    wallet.address,
+    0,
+    {
+      from: wallet.address,
+      gasLimit: gasLimit,
+    }
+  );
+  console.log("Supplied");
+  console.log("TX: ", supTx);
 }
-ethers.getContractAtFromArtifact();
-main(
-  "0xE039BdF1d874d27338e09B55CB09879Dedca52D8",
-  "0x57ac4E23aE911Cb3aEDAfE9ABb8E68a68F7CC463"
-).catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+
+const rinkebyWETH = "0xd74047010D77c5901df5b0f9ca518aED56C85e8D";
+const rinkebyWBTC = "0x124F70a8a3246F177b0067F435f5691Ee4e467DD";
+main("0xE039BdF1d874d27338e09B55CB09879Dedca52D8", rinkebyWBTC).catch(
+  (error) => {
+    console.error(error);
+    process.exitCode = 1;
+  }
+);
