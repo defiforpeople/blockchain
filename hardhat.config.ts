cc143c122708bec8bcd093e80689efc4679f439c
NicoBarragan <nbarragan01@gmail.com>
2022-05-13 03:45:24 -0300

    feat(hardhat.config.ts): implemented a selection of the network from the console

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,14 @@ import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
-dotenv.config();
+const found = process.argv.indexOf("--network");
+const networkName = process.argv[found + 1];
+if (!networkName) {
+  throw new Error("invalid network name");
+}
+dotenv.config({
+  path: `.env.${networkName}`,
+});
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -41,12 +48,12 @@ const config: HardhatUserConfig = {
   },
   networks: {
     ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+      url: process.env.URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     rinkeby: {
-      url: process.env.RINKEBY_URL || "",
+      url: process.env.URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
