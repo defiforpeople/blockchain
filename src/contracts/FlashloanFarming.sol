4a81aeffac006efc2f949e371dcdf5bbe9cbb801
NicoBarragan <nbarragan01@gmail.com>
2022-05-17 20:21:31 -0300

    feat(FlashloanFarming.sol): WIP doesn't compile yet

--- src/contracts/FlashloanFarming.sol
+++ src/contracts/FlashloanFarming.sol
@@ -1,83 +1,117 @@
-// // SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
 
-// pragma solidity ^0.8.0;
+import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeMath} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/SafeMath.sol";
 
-// contract FlashloanFarming {
-//     // is FlashLoanReceiverBase {
-//     uint256 public tokenPercentage;
-//     uint256 public totalSupplyAmount;
-//     uint256 public flashloanAmount;
-//     uint256 public ltv;
-//     uint256 public maxAvailableAmount;
-//     address public aavePool;
+contract FlashloanFarming is FlashLoanSimpleReceiverBase {
+    using SafeMath for uint256;
 
-//     function calculateFlashloanAmount(
-//         uint256 _amount,
-//         uint256 _ltv,
-//         uint256 _flashloanFee
-//     ) internal returns (uint256) {
-//         tokenPercentage = 100 - (_ltv * 100);
-//         totalSupplyAmount = (_amount * tokenPercentage) / 100;
-//         flashloanAmount = (_ltv * totalSupplyAmount) / 100;
-//         return flashloanAmount - _flashloanFee;
-//     }
+    uint256 private tokenPercentage;
+    uint256 private totalSupplyAmount;
+    uint256 private flashloanAmount;
+    uint256 private flashloanFee;
+    uint256 private ltv;
+    uint256 private amount;
+    uint256 private maxAvailableAmount;
+    // address private POOL;
+    address private tokenAddr;
+    bytes32 private aaveTokenResponse;
 
-//     function askFlashloan(address tokenAddr, uint256 _amount) internal {
-//         // Ask flashloan on Aave
-//         // flashLoan( address receiverAddress, address[] calldata assets, uint256[] calldata amounts, uint256[] interestRateModes, address onBehalfOf, bytes calldata params, uint16 referralCode)
-//     }
+    constructor(IPoolAddressesProvider _addressProvider)
+        FlashLoanSimpleReceiverBase(_addressProvider)
+    {}
 
-//     function repayFlashloan(address tokenAddr, uint256 _amount) internal {
-//         // Give back the flashloan
-//     }
+    function executeOperation(
+        address asset,
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata params
+    ) external returns (bool) {
+        uint256 amountOwing = amount.add(premium);
+        IERC20(asset).approve(address(POOL), amountOwing);
 
-//     event Success(
-//         address tokenAddr,
-//         uint256 userAddr,
-//         uint256 amount,
-//         bool success
-//     );
+        return true;
+    }
 
-//     struct Investment {
-//         address tokenAddr;
-//         uint256 amount;
-//         uint256 flashloanAmount;
-//         bool success;
-//     }
-//     mapping(address => Investment) public investments;
+    function askFlashloan(address _asset, uint256 _amount) internal {
+        address receiverAddress = address(this);
+        bytes memory params = "";
+        uint16 referralCode = 0;
+        POOL.flashLoanSimple(
+            receiverAddress,
+            _asset,
+            _amount,
+            params,
+            referralCode
+        );
+    }
 
-//     function startStrategy(address tokenAddr, uint256 _amount) external {
-//         require(_amount > 0, "Amount must be greater than 0");
-//         (ltv, maxAvailableAmount) = getConfiguration(tokenAddr);
-//         require(
-//             ltv > 0 && maxAvailableAmount > 0,
-//             "LTV & available liquidity must be greater than 0"
-//         );
+    function calculateFlashloanAmount(
+        uint256 _amount,
+        uint256 _ltv,
+        uint256 _flashloanFee
+    ) internal returns (uint256) {
+        tokenPercentage = 100 - (_ltv * 100);
+        totalSupplyAmount = (_amount * tokenPercentage) / 100;
+        flashloanAmount = (_ltv * totalSupplyAmount) / 100;
+        return flashloanAmount - _flashloanFee;
+    }
 
-//         flashloanFee = aavePool.FLASHLOAN_PREMIUM_TOTAL();
-//         flashloanAmount = calculateFlashloanAmount(_amount, ltv, flashloanFee);
-//         askFlashloan(tokenAddr, flashloanAmount);
-//         // aavePool.supplyWithPermit(tokenAddr, _amount + flashloanAmount, address(this), 0, uint256 deadline, uint8 permitV, bytes32 permitR, bytes32 permitS);
-//         aavePool.borrow(tokenAddr, FlashloanAmount, 2, 0, address(this));
-//         repayFlashloan(tokenAddr, FlashloanAmount);
+    event Success(
+        address tokenAddr,
+        uint256 userAddr,
+        uint256 amount,
+        bool success
+    );
 
-//         emit Success(tokenAddr, msg.sender, _amount + flashloanAmount, true);
-//         investments[msg.sender] = Investment(
-//             tokenAddr,
-//             _amount,
-//             flashloanAmount,
-//             true
-//         );
-//     }
+    struct Investment {
+        address tokenAddr;
+        uint256 amount;
+        uint256 flashloanAmount;
+        bool success;
+    }
+    mapping(address => Investment) public investments;
 
-//     function closeStrategy(address userAddr) external {
-//         require(investments[msg.sender], "Investment not found");
-//         (tokenAddr, amount, flashloanAmount, _) = investments[msg.sender];
-//         askFlashloan(tokenAddr, flashloanAmount);
-//         aavePool.repayWithATokens(tokenAddr, flashloanAmount, 2);
+    function startStrategy(address _tokenAddr, uint256 _amount) external {
+        require(_amount > 0, "Amount must be greater than 0");
+        // (ltv, maxAvailableAmount) = POOL.getConfiguration(_tokenAddr);
+        aaveTokenResponse = POOL.getConfiguration(_tokenAddr);
+        require(
+            ltv > 0 && maxAvailableAmount > 0,
+            "LTV & available liquidity must be greater than 0"
+        );
 
-//         aavePool.withdraw(tokenAddr, amount + flashloanAmount, address(this));
-//         repayFlashloan(tokenAddr, flashloanAmount);
-//         token.transfer(userAddr, amount);
-//     }
-// }
+        flashloanFee = POOL.FLASHLOAN_PREMIUM_TOTAL();
+        flashloanAmount = calculateFlashloanAmount(_amount, ltv, flashloanFee);
+        askFlashloan(_tokenAddr, flashloanAmount);
+        POOL.supplyWithPermit(
+            _tokenAddr,
+            _amount + flashloanAmount,
+            address(this),
+            0
+        ); // uint256 deadline, uint8 permitV, bytes32 permitR, bytes32 permitS);
+        POOL.borrow(_tokenAddr, flashloanAmount, 2, 0, address(this));
+
+        emit Success(_tokenAddr, msg.sender, _amount + flashloanAmount, true);
+        investments[msg.sender] = Investment(
+            _tokenAddr,
+            _amount,
+            flashloanAmount,
+            true
+        );
+    }
+
+    function closeStrategy(address userAddr) external {
+        require(investments[msg.sender], "Investment not found");
+        (tokenAddr, amount, flashloanAmount, , ) = investments[msg.sender];
+        askFlashloan(tokenAddr, flashloanAmount);
+        POOL.repayWithATokens(tokenAddr, flashloanAmount, 2);
+
+        POOL.withdraw(tokenAddr, amount + flashloanAmount, address(this));
+        IERC20(tokenAddr).transfer(userAddr, amount);
+    }
+}
