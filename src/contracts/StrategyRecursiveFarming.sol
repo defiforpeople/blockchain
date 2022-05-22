8eb0dec2cc9bc47d56f80509b2f43519bbd963cd
NicoBarragan <nbarragan01@gmail.com>
2022-05-22 02:52:02 -0300

    refactor(StrategyRecursiveFarming.sol): we get WAVAX token address in the constructor, tokenaddr is not necessary in parameters of functions

--- src/contracts/StrategyRecursiveFarming.sol
+++ src/contracts/StrategyRecursiveFarming.sol
@@ -20,21 +20,29 @@ contract StrategyRecursiveFarming is
     KeeperCompatibleInterface
 {
     using EnumerableSet for EnumerableSet.AddressSet;
+    IERC20 public token;
     IPool private aavePool;
-    bool private continues;
     DataTypes.ReserveConfigurationMap private tokenInfo;
-    AggregatorV3Interface private gasPriceFeed;
     uint256 private ltv;
+    AggregatorV3Interface private gasPriceFeed;
+    bool private continues;
     uint256 private investmentAmount;
     uint256 private constant GAS_USED_DEPOSIT = 1074040;
     uint256 private constant GAS_USED_SUPPLY = 10740;
     uint256 private constant GAS_USED_BORROW = 10740;
     uint256 private constant INTEREST_RATE_MODE = 2;
+    // timestamp save the last time a contract loop was executed
+    uint256 public lastTimestamp;
 
-    constructor(address _aavePoolAddr, address _gasPriceFeedAddress) {
+    constructor(
+        address _aavePoolAddr,
+        address _gasPriceFeedAddr,
+        address _wavaxAddr
+    ) {
         lastTimestamp = block.timestamp;
         aavePool = IPool(_aavePoolAddr);
-        gasPriceFeed = AggregatorV3Interface(_gasPriceFeedAddress);
+        gasPriceFeed = AggregatorV3Interface(_gasPriceFeedAddr);
+        token = IERC20(_wavaxAddr);
     }
 
     enum StrategyStatus {
@@ -54,38 +62,15 @@ contract StrategyRecursiveFarming is
         InvestStatus status;
     }
 
-    struct Token {
-        string name;
-        address addr;
-        address priceFeedAddr;
-    }
-
     // define investments information
     mapping(address => Invest) public _investments;
     EnumerableSet.AddressSet private _investmentsAddrs;
 
-    // define allowed tokens
-    mapping(address => Token) public _tokens;
-    EnumerableSet.AddressSet private _tokensAddrs;
-
-    // timestamp save the last time a contract loop was executed
-    uint256 public lastTimestamp;
-
     // define contracts events
-    event Deposit(
-        address userAddr,
-        address tokenAddr,
-        uint256 amount,
-        uint256 quotas
-    );
-    event Borrow(address userAddr, address tokenAddr, uint256 amount);
-    event Supply(
-        address userAddr,
-        address tokenAddr,
-        uint256 amount,
-        bool continues
-    );
-    event Withdraw(address userAddr, address tokenAddr, uint256 quotas);
+    event Deposit(address userAddr, uint256 amount, uint256 quotas);
+    event Borrow(address userAddr, uint256 amount);
+    event Supply(address userAddr, uint256 amount, bool continues);
+    event Withdraw(address userAddr, uint256 quotas);
 
     // method for calculating supply amount, by substracting the gas amount estimation
     function calculateSupplyAmount(uint256 totalAmount)
@@ -103,28 +88,22 @@ contract StrategyRecursiveFarming is
     }
 
     // method for unwrap the ERC20 into the native token, in order to pay gas with that
-    function _unwrapERC20Token(address tokenAddr, uint256 amount) internal {
-        IWETH(tokenAddr).withdraw(amount);
+    function _unwrapERC20Token(uint256 amount) internal {
+        IWETH(address(token)).withdraw(amount);
     }
 
     // method for getting the Loan To Value (LTV) of the asset
-    function getLTV(address tokenAddr) internal returns (uint256) {
-        tokenInfo = aavePool.getConfiguration(tokenAddr);
+    function getLTV() internal returns (uint256) {
+        tokenInfo = aavePool.getConfiguration(address(token));
+        //tokenInfo = 5708990770823839524233143896245666479610309254976
         // TODO(nb): Parse ltv from tokenInfo or change implementation
         ltv = 200000; // tokenInfo[:15]
         return ltv;
     }
 
-    function _getCuotaQty(address tokenAddr, uint256 amount)
-        internal
-        view
-        returns (uint256)
-    {
-        IERC20 token = IERC20(tokenAddr);
+    function _getCuotaQty(uint256 amount) internal view returns (uint256) {
         console.log("balance: ", token.balanceOf(address(this)));
-
         uint256 qty = amount / _getCuotaPrice();
-
         return qty;
     }
 
@@ -133,16 +112,7 @@ contract StrategyRecursiveFarming is
     }
 
     // method defined for the user can make an investment, whether it is a first time or not
-    function deposit(
-        address userAddr,
-        address tokenAddr,
-        uint256 amount
-    ) external payable {
-        // check if token is supported by strategy
-        require(_tokensAddrs.contains(tokenAddr), "invalid token");
-
-        // check if user balance is enough
-        IERC20 token = IERC20(tokenAddr);
+    function deposit(address userAddr, uint256 amount) external payable {
         require(
             amount <= token.balanceOf(address(this)),
             "balance is not enough"
@@ -156,7 +126,7 @@ contract StrategyRecursiveFarming is
             i.total = amount;
             i.neto = 0;
             i.status = InvestStatus.Active;
-            i.quotas = _getCuotaQty(tokenAddr, amount);
+            i.quotas = _getCuotaQty(amount);
 
             _investmentsAddrs.add(msg.sender);
         }
@@ -166,39 +136,30 @@ contract StrategyRecursiveFarming is
         if (amount - investmentAmount < address(this).balance) {
             // if we don't, we'll unwrap the necessary gas amount of the ERC20 token
             _unwrapERC20Token(
-                tokenAddr,
                 amount - investmentAmount - address(this).balance
             );
         }
         // approve and supply liquidity to the protocol
         token.approve(address(aavePool), investmentAmount);
-        aavePool.supply(tokenAddr, investmentAmount, address(this), 0);
+        aavePool.supply(address(token), investmentAmount, address(this), 0);
 
         // get the LTV value for the function borrow() to work correctly
-        ltv = getLTV(tokenAddr);
-        emit Deposit(userAddr, tokenAddr, investmentAmount, i.quotas);
+        ltv = getLTV();
+        emit Deposit(userAddr, investmentAmount, i.quotas);
     }
 
-    function borrow(
-        address userAddr,
-        address tokenAddr,
-        uint256 amount
-    ) external {
+    function borrow(address userAddr, uint256 amount) external {
         aavePool.borrow(
-            tokenAddr,
+            address(token),
             amount * ltv,
             INTEREST_RATE_MODE,
             0,
             address(this)
         );
-        emit Borrow(userAddr, tokenAddr, amount * ltv);
+        emit Borrow(userAddr, amount * ltv);
     }
 
-    function supply(
-        address userAddr,
-        address tokenAddr,
-        uint256 amount
-    ) external {
+    function supply(address userAddr, uint256 amount) external {
         continues = true;
         (, int256 gasPrice, , , ) = gasPriceFeed.latestRoundData();
         console.logInt(gasPrice);
@@ -212,26 +173,22 @@ contract StrategyRecursiveFarming is
             _investments[userAddr].neto = _investments[userAddr].total - amount;
         }
 
-        IERC20(tokenAddr).approve(address(aavePool), amount);
-        aavePool.supply(tokenAddr, amount, address(this), 0);
+        token.approve(address(aavePool), amount);
+        aavePool.supply(address(token), amount, address(this), 0);
 
-        emit Supply(userAddr, tokenAddr, amount, continues);
+        emit Supply(userAddr, amount, continues);
     }
 
     // method for repay the borrow with collateral
-    function repayWithCollateral(address tokenAddr, uint256 amount) public {
-        aavePool.repayWithATokens(tokenAddr, amount, INTEREST_RATE_MODE);
+    function repayWithCollateral(uint256 amount) public {
+        aavePool.repayWithATokens(address(token), amount, INTEREST_RATE_MODE);
     }
 
     // method defined for the user can withdraw from the strategy
     function withdraw(
         address userAddr, // @nb: Can we avoid this param using msg.sender??
-        address tokenAddr,
         uint256 quotas
     ) external payable {
-        // check if token is supported by strategy
-        require(_tokensAddrs.contains(tokenAddr), "invalid token");
-
         // get current investment by user address
         Invest storage i = _investments[userAddr];
 
@@ -248,48 +205,21 @@ contract StrategyRecursiveFarming is
 
         // TODO(ca): transfer amount (quota*price) to user address
 
-        emit Withdraw(userAddr, tokenAddr, quotas);
+        emit Withdraw(userAddr, quotas);
     }
 
-    function transferUser(
-        address userAddr,
-        address tokenAddr,
-        uint256 quotas
-    ) external {
+    function transferUser(address userAddr, uint256 quotas) external {
         // TODO: make quotas * amount calculation
-        IERC20(tokenAddr).transfer(userAddr, quotas);
+        token.transfer(userAddr, quotas);
     }
 
-    // @nb: if we use only the native token, we don't need this 2 functions
-    // method defined to add support to a new erc20 token for the strategy
-    function addToken(
-        string memory name,
-        address tokenAddr,
-        address priceFeedAddr
-    ) external {
-        require(!_tokensAddrs.contains(tokenAddr), "token already exists");
-
-        Token storage t = _tokens[tokenAddr];
-        t.name = name;
-        t.addr = tokenAddr;
-        t.priceFeedAddr = priceFeedAddr;
-
-        _tokensAddrs.add(tokenAddr);
-    }
-
-    // method defined to remove a token from the strategy
-    function removeToken(address tokenAddr) external {
-        delete _tokens[tokenAddr];
-
-        _tokensAddrs.remove(tokenAddr);
-    }
-
+    // @nb: remove parameter tokenAddr from IStraetgy? Is not necessary here.
     function getQuotaQty(address tokenAddr, uint256 amount)
         external
         view
         returns (uint256)
     {
-        return _getCuotaQty(tokenAddr, amount);
+        return _getCuotaQty(amount);
     }
 
     function getQuotaPrice() external pure override returns (uint256) {
