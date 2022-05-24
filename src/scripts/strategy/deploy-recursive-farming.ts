ffa3c444c4c53e2b3d72276a6191f60cf4c40768
NicoBarragan <nbarragan01@gmail.com>
2022-05-24 20:14:16 -0300

    refactor(deploy-recursive-farming.ts): changed WETH address for WAVAX address as the constructor param

--- src/scripts/strategy/deploy-recursive-farming.ts
+++ src/scripts/strategy/deploy-recursive-farming.ts
@@ -1,13 +1,21 @@
 import { ethers } from "hardhat";
 import "@nomiclabs/hardhat-ethers";
-import * as strategy from "../../utils/strategy/recursive-farming";
 const logger = require("pino")();
+
+const { WAVAX_ADDRESS, AAVE_POOL_ADDRESS, GAS_DATA_FEED } = process.env;
 
 // run script
 (async () => {
   try {
     logger.info(`Deploying the contract...`);
-    const contract = await strategy.deployContract(ethers);
+    const StrategyContract = await ethers.getContractFactory(
+      "StrategyRecursiveFarming"
+    );
+    const contract = await StrategyContract.deploy(
+      AAVE_POOL_ADDRESS,
+      GAS_DATA_FEED,
+      WAVAX_ADDRESS
+    );
     logger.info(`Strategy contract deployed to: ${contract.address}`);
   } catch (err) {
     logger.error(err);
