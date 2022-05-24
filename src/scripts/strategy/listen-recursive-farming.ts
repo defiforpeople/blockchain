fe3016d85af21d22025c5cc3f2a57b2b0189ddaa
NicoBarragan <nbarragan01@gmail.com>
2022-05-24 20:16:00 -0300

    feat(listen-recursive-farming.ts): changed implementation for working as a backend, managing the execution flow of the contract

--- src/scripts/strategy/listen-recursive-farming.ts
+++ src/scripts/strategy/listen-recursive-farming.ts
@@ -1,93 +1,163 @@
 import { ethers } from "hardhat";
 import "@nomiclabs/hardhat-ethers";
 import { BigNumber } from "ethers";
-import { StrategyRecursiveFarming } from "../../typechain";
+// eslint-disable-next-line camelcase
+import {
+  // eslint-disable-next-line camelcase
+  IERC20__factory,
+  IERC20,
+  StrategyRecursiveFarming,
+} from "../../typechain";
 const logger = require("pino")();
 
+// amount swapped: 0.0001 ether
+
+const { WETH_ADDRESS } = process.env;
+const CONTRACT_ADDRESS = "0x1586a098746EeC3e1210ca06a582f550a7dA431C";
 const GAS_LIMIT = 2074040;
-export async function listen(strategyAddress: string): Promise<void> {
-  const strategyContract = ethers.getContractFactory(
-    "StrategyRecursiveFarming"
-  );
-  const strategy = (await strategyContract).attach(
-    strategyAddress
-  ) as StrategyRecursiveFarming;
+const AMOUNT = 10000000000;
+const [WALLET] = await ethers.getSigners();
 
-  strategy.on(
-    "Deposit",
-    async (userAddr: string, amount: BigNumber, quotas: BigNumber, ev: any) => {
-      try {
-        const borrowTx = await strategy.borrow(userAddr, amount, {
-          from: strategy.address,
-          gasLimit: GAS_LIMIT,
-        });
-        await borrowTx.wait();
-      } catch (err) {
-        logger.error(err);
-      }
+const STRATEGY_CONTRACT = await ethers.getContractFactory(
+  "StrategyRecursiveFarming"
+);
+
+const STRATEGY = (await STRATEGY_CONTRACT.attach(
+  CONTRACT_ADDRESS
+)) as StrategyRecursiveFarming;
+
+// define instance of erc20 token
+const token = (await ethers.getContractAt(
+  IERC20__factory.abi,
+  `${WETH_ADDRESS}`
+)) as IERC20;
+
+const doDeposit = async (): Promise<void> => {
+  // make approve tx to erc20 token contract and wait confirmation
+  await logger.info("Approving...");
+  const tx = await token.approve(CONTRACT_ADDRESS, AMOUNT, {
+    from: WALLET.address,
+    gasLimit: GAS_LIMIT,
+  });
+  tx.wait();
+  await logger.info("Approved...");
+
+  await logger.info("Supplying...");
+  const depositTx = await STRATEGY.deposit(AMOUNT, {
+    from: WALLET.address,
+    gasLimit: GAS_LIMIT,
+  });
+  depositTx.wait();
+  await logger.info("Supplied...");
+  console.log(depositTx);
+
+  doRecursion();
+};
+
+const doRecursion = async (): Promise<void> => {
+  try {
+    const DONE = 2;
+
+    if ((await STRATEGY.viewStatus()) === DONE) {
+      return;
     }
-  );
 
-  strategy.on(
-    "Borrow",
-    async (userAddr: string, amount: BigNumber, ev: any): Promise<void> => {
-      try {
-        const supplyTx = await strategy.supply(userAddr, amount, {
-          from: strategy.address,
-          gasLimit: GAS_LIMIT,
-        });
-        await supplyTx.wait();
-      } catch (err) {
-        logger.error(err);
-      }
+    if (
+      (await STRATEGY.gasNeeded({
+        from: WALLET.address,
+        gasLimit: GAS_LIMIT,
+      })) > BigNumber.from("0")
+    ) {
+      // TODO(nb): method for adding gas to the wallet owner
+      return;
     }
-  );
 
-  strategy.on(
-    "Supply",
-    async (
-      userAddr: string,
-      amount: BigNumber,
-      continues: boolean,
-      ev: any
-    ): Promise<void> => {
-      try {
-        if (!continues) {
-          return;
-        }
-        const borrowTx = await strategy.borrow(userAddr, amount);
-        await borrowTx.wait();
-      } catch (err) {
-        logger.error(err);
-      }
-    }
-  );
+    const tx = await STRATEGY.doRecursion({
+      from: WALLET.address,
+      gasLimit: GAS_LIMIT,
+    });
+    await tx.wait();
+  } catch (err) {
+    logger.error(err);
+  }
+};
 
-  strategy.on(
-    "InitWithdraw",
-    async (userAddr: string, amount: BigNumber): Promise<void> => {
-      try {
-        const repayTx = await strategy.repayWithCollateral(amount);
-        await repayTx.wait();
+const reqWithdraw = async (
+  userAddr: string,
+  amount: BigNumber
+): Promise<void> => {
+  const reqTx = await STRATEGY.requestWithdraw(amount, {
+    from: userAddr,
+    gasLimit: GAS_LIMIT,
+  });
+  await reqTx.wait();
+  console.log(reqTx);
 
-        const withdrawTx = await strategy.withdraw(userAddr, amount);
-        await withdrawTx.wait();
-      } catch (err) {
-        logger.error(err);
-      }
-    }
-  );
+  // TODO(nb): try to do it with events
+  const withdrawTx = await STRATEGY._withdraw(userAddr, amount, {
+    from: WALLET.address,
+    // TODO(nb): question: is possible to sign this tx with the user addres too?
+    gasLimit: GAS_LIMIT,
+  });
+  await withdrawTx.wait();
+  console.log(withdrawTx);
+};
 
-  strategy.on(
-    "Withdraw",
-    async (userAddr: string, quotas: BigNumber): Promise<void> => {
-      try {
-        const transferTx = await strategy.transferUser(userAddr, quotas);
-        await transferTx.wait();
-        return;
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  );
-}
+// We´ll cover the gas of the off-chain and sc interactions
+const main = async () => {
+  await doDeposit(); // Don't have more amount :(
+
+  await reqWithdraw(WALLET.address, BigNumber.from("100"));
+};
+
+main();
+
+// strategy.on(
+//   "Withdraw",
+//   async (userAddr: string, amount: BigNumber): Promise<void> => {
+//     try {
+//       const tx = await strategy.performUpkeep("", {
+//         from: strategy.address,
+//         gasLimit: GAS_LIMIT,
+//       });
+//       await tx.wait();
+//     } catch (err) {
+//       logger.error(err);
+//     }
+//   }
+// );
+
+// strategy.on("Borrow", async (ev: any): Promise<void> => {
+//   try {
+//     const supplyTx = await strategy.supply({
+//       from: strategy.address,
+//       gasLimit: GAS_LIMIT,
+//     });
+//     await supplyTx.wait();
+//   } catch (err) {
+//     logger.error(err);
+//   }
+// });
+// strategy.on("Deposit", async (ev: any): Promise<void> => {
+//   try {
+//     const borrowTx = await strategy.borrow();
+//     await borrowTx.wait();
+//   } catch (err) {
+//     logger.error(err);
+//   }
+// });
+// };
+
+// strategy.on("Deposit", async (ev: any): Promise<void> => {
+//   console.log("Listening event");
+//   try {
+//     console.log("Performing keeper: ");
+//     const borrowTx = await strategy.performUpkeep("", {
+//       from: wallet.address,
+//       gasLimit: GAS_LIMIT,
+//     });
+//     await borrowTx.wait();
+//   } catch (err) {
+//     logger.error(err);
+//   }
+// });
