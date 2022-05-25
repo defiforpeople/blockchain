0e577b6921ad25bbe21c7d9e3227cd8cbeada2f7
NicoBarragan <nbarragan01@gmail.com>
2022-05-25 04:07:36 -0300

    feat(hardhat.config.ts): added fuji network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -220,6 +220,11 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    fuji: {
+      url: process.env.URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
