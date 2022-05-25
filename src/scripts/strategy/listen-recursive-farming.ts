bd50a32c91f8f5b3c80d81090a7f38816ca4a813
NicoBarragan <nbarragan01@gmail.com>
2022-05-25 18:11:19 -0300

    refactor(listen-recursive-farming.ts): organized better the code and added comments explaining the code

--- src/scripts/strategy/listen-recursive-farming.ts
+++ src/scripts/strategy/listen-recursive-farming.ts
@@ -15,33 +15,55 @@ import { getWeth } from "../../utils/helpers/get-weth";
 import { linkFund } from "../../utils/helpers/link-fund";
 const logger = require("pino")();
 
+// defined constants
 const { WRAPPED_NATIVE_TOKEN_ADDRESS, CONTRACT_ADDRESS, LINK_ADDRESS } =
   process.env;
 const GAS_LIMIT = 2074040;
 const AMOUNT = BigNumber.from(1000000000);
+const WITHDRAW_AMOUNT = BigNumber.from(100);
 
+// method for executing recursion based on the contract status (supply or borrow)
 const doRecursion = async (): Promise<void> => {
+  // get the owner wallet
   const [wallet] = await ethers.getSigners();
+
+  // get the contract
   const strategyCONTRACT = await ethers.getContractFactory(
     "StrategyRecursiveFarming"
   );
-
   const strategy = (await strategyCONTRACT.attach(
     `${CONTRACT_ADDRESS}`
   )) as StrategyRecursiveFarming;
 
+  // get the LINK token
   const linkToken = (await ethers.getContractAt(
     LinkTokenInterface__factory.abi,
     `${LINK_ADDRESS}`
   )) as LinkTokenInterface;
 
+  // evaluating LINK amount of the wallet owner before executing (for testing)
+  let contractLinkAmm = await linkToken.balanceOf(wallet.address);
+  await logger.info(
+    `the wallet amount of LINK after recursion is: ${contractLinkAmm}`
+  );
+
+  // evaluating LINK amount of the smart contract before executing (for testing)
+  let walletLinkAmm = await linkToken.balanceOf(`${CONTRACT_ADDRESS}`);
+  await logger.info(
+    `the wallet amount of LINK after recursion is: ${walletLinkAmm}`
+  );
+
   try {
+    // define constants for possible smart contract status
     const borrow = 0;
     const supply = 1;
     const done = 2;
 
+    // print the strategy status
     let strategyStatus = await strategy.viewStatus();
     await logger.info(`The status of the strategy is ${strategyStatus}`);
+
+    // if the status is done, recursion is not necessary
     if (strategyStatus === done) {
       logger.info("The strategy is in DONE status");
       return;
@@ -75,6 +97,7 @@ const doRecursion = async (): Promise<void> => {
     //   return;
     // }
 
+    // start execution of the recursion
     await logger.info("Executing Recursion function...");
     const tx = await strategy.doRecursion({
       from: wallet.address,
@@ -82,9 +105,11 @@ const doRecursion = async (): Promise<void> => {
     });
     await tx.wait();
 
+    // get and print the strategy status
     strategyStatus = await strategy.viewStatus();
     await logger.info(`The status of the strategy is ${strategyStatus}`);
 
+    // if the status if supply, then execute again doRecursion()
     if (strategyStatus === supply) {
       await logger.info("Executing Supply...");
       const tx = await strategy.doRecursion({
@@ -93,92 +118,127 @@ const doRecursion = async (): Promise<void> => {
       });
       await tx.wait();
     }
+
+    // get and print the strategy status
     strategyStatus = await strategy.viewStatus();
     await logger.info(`The status of the strategy is ${strategyStatus}`);
+
+    // evaluating LINK amount of the wallet owner after executing (for testing)
+    contractLinkAmm = await linkToken.balanceOf(wallet.address);
+    await logger.info(
+      `the wallet amount of LINK after recursion is: ${contractLinkAmm}`
+    );
+
+    // evaluating LINK amount of the smart contract after executing (for testing)
+    walletLinkAmm = await linkToken.balanceOf(`${CONTRACT_ADDRESS}`);
+    await logger.info(
+      `the wallet amount of LINK after recursion is: ${walletLinkAmm}`
+    );
   } catch (err) {
     logger.error(err);
   }
 };
 
-const doDeposit = async (amount: BigNumber): Promise<void> => {
-  const [wallet] = await ethers.getSigners();
+// function for user to deposit in the smart contract and start strategy
+const doDeposit = async (
+  userAddr: string,
+  amount: BigNumber
+): Promise<void> => {
+  // get the strategy contract
   const strategyContract = await ethers.getContractFactory(
     "StrategyRecursiveFarming"
   );
-
-  const strategy = (await strategyContract.attach(
+  const strategy = strategyContract.attach(
     `${CONTRACT_ADDRESS}`
-  )) as StrategyRecursiveFarming;
+  ) as StrategyRecursiveFarming;
 
   // define instance of erc20 token
   const token = (await ethers.getContractAt(
     IERC20__factory.abi,
     `${WRAPPED_NATIVE_TOKEN_ADDRESS}`
   )) as IERC20;
+
   // make approve tx to erc20 token contract and wait confirmation
   await logger.info("Approving...");
   const tx = await token.approve(`${CONTRACT_ADDRESS}`, amount, {
-    from: wallet.address,
+    from: userAddr,
     gasLimit: GAS_LIMIT,
   });
   await tx.wait();
   await logger.info("Approved...");
 
-  await logger.info("Supplying...");
+  // execute deposit() in the strategy contract
+  await logger.info("Depositing...");
   const depositTx = await strategy.deposit(amount, {
-    from: wallet.address,
+    from: userAddr,
     gasLimit: GAS_LIMIT,
   });
   await depositTx.wait();
-  await logger.info("Supplied...");
+  await logger.info("Deposited...");
 
   logger.info("Going to Recursion function....");
+  // call function for executing recursion
   await doRecursion();
 };
 
+// function for user to request withdraw
 const reqWithdraw = async (
   userAddr: string,
   amount: BigNumber
 ): Promise<void> => {
+  // get the wallet owner
   const [wallet] = await ethers.getSigners();
+
+  // get strategy contract
   const strategyCONTRACT = await ethers.getContractFactory(
     "StrategyRecursiveFarming"
   );
-
   const strategy = (await strategyCONTRACT.attach(
     `${CONTRACT_ADDRESS}`
   )) as StrategyRecursiveFarming;
 
-  logger.info("Executing requestWithdraw");
-  const reqTx = await strategy.requestWithdraw(amount, {
-    from: userAddr,
-    gasLimit: GAS_LIMIT,
-  });
-  await reqTx.wait();
-  await logger.info(reqTx);
+  try {
+    // execute requestWithdraw from user address and with the amount to withdraw
+    logger.info("Executing requestWithdraw");
+    const reqTx = await strategy.requestWithdraw(amount, {
+      from: userAddr,
+      gasLimit: GAS_LIMIT,
+    });
+    await reqTx.wait();
 
-  await logger.info("Executing Withdraw");
-  // TODO(nb): try to do it with events
-  const withdrawTx = await strategy.withdraw(userAddr, amount, {
-    from: wallet.address,
-    // TODO(nb): question: is possible to sign this tx with the user address too?
-    gasLimit: GAS_LIMIT,
-  });
-  await withdrawTx.wait();
-  await logger.info(withdrawTx);
+    // after it finishes, it calls the withdraw function in the contract for withdraw that amount of aave ans transfer it to user address
+    await logger.info("Executing Withdraw");
+    // TODO(nb): try to do it with events
+    const withdrawTx = await strategy.withdraw(userAddr, amount, {
+      from: wallet.address,
+      // TODO(nb): question: is possible to sign this tx with the user address too?
+      gasLimit: GAS_LIMIT,
+    });
+    await withdrawTx.wait();
+    await logger.info(withdrawTx);
+  } catch (err) {
+    logger.error(err);
+  }
 };
 
+// function for managing the execution flow of the other functions
 const main = async () => {
+  // get the owner wallet
   const [wallet] = await ethers.getSigners();
 
+  // get the ERC20 native token (WAVAX in this case)
   const token = (await ethers.getContractAt(
     IERC20__factory.abi,
     `${WRAPPED_NATIVE_TOKEN_ADDRESS}`
   )) as IERC20;
 
-  const wavaxBalance = await token.balanceOf(wallet.address);
-  await logger.info(`Our current amount of WAVAX is ${wavaxBalance}`);
+  // get and print WAVAX balance before executing
+  let wavaxBalance = await token.balanceOf(wallet.address);
+  await logger.info(
+    `Our amount of WAVAX in the wallet before execution is ${wavaxBalance}`
+  );
 
+  // if we don't have enough WAVAX in the wallet, we'll wrapp it from our AVAX
   if (wavaxBalance < AMOUNT) {
     await logger.info("Getting WAVAX...");
     await getWeth(wallet.address, AMOUNT.sub(wavaxBalance));
@@ -186,11 +246,73 @@ const main = async () => {
       `WAVAX tx completed, our amount is: ${token.balanceOf(wallet.address)}`
     );
   }
-  await logger.info("Doing Deposit...");
-  await doDeposit(AMOUNT); // Don't have more amount :(
 
+  // execute Deposit function from the address sender (in this case, the owner wallet)
+  await logger.info("Doing Deposit...");
+  await doDeposit(wallet.address, AMOUNT); // Don't have more amount :(
+
+  // execute withdraw from the address sender (in this case, the owner wallet)
   await logger.info("Requesting Withdraw...");
-  await reqWithdraw(wallet.address, BigNumber.from("100"));
+  await reqWithdraw(wallet.address, WITHDRAW_AMOUNT);
+
+  // get and print WAVAX balance after executing
+  wavaxBalance = await token.balanceOf(wallet.address);
+  await logger.info(
+    `Our amount of WAVAX in the wallet after execution is ${wavaxBalance}`
+  );
 };
 
+// TODO(nb): implement timer for execuitng doRecursion() automatically
 main();
+
+// ## UNUSED METHODS: they are for checking things:
+// method for claiming rewards tokens from aave to the strategy contract
+const claimRewardToken = async () => {
+  // get owner wallet
+  const [wallet] = await ethers.getSigners();
+
+  // get strategy contract
+  const strategyCONTRACT = await ethers.getContractFactory(
+    "StrategyRecursiveFarming"
+  );
+  const strategy = (await strategyCONTRACT.attach(
+    `${CONTRACT_ADDRESS}`
+  )) as StrategyRecursiveFarming;
+
+  try {
+    // claim the rewards from aave
+    logger.info("Claiming rewards ...");
+    const tx = await strategy.claimRewards({
+      from: wallet.address,
+      gasLimit: GAS_LIMIT,
+    });
+    await tx.wait();
+    logger.info(`rewards claimed`);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// method for watching gas price from chainlink data feed (for testing)
+const watchGasPrice = async () => {
+  // get strategy contract
+  const strategyCONTRACT = await ethers.getContractFactory(
+    "StrategyRecursiveFarming"
+  );
+  const strategy = (await strategyCONTRACT.attach(
+    `${CONTRACT_ADDRESS}`
+  )) as StrategyRecursiveFarming;
+
+  try {
+    // execute method that returns gas price from the strategy contract
+    const gasPrice = await strategy.gasPrice();
+
+    // print it
+    logger.info(BigNumber.from(gasPrice));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// watchGasPrice();
+// claimRewardToken();
