093e89b0e6698d27f708462277137be9960737a5
NicoBarragan <nbarragan01@gmail.com>
2022-05-27 20:18:48 -0300

    refactor(re-withdraw.ts): renamed file

--- src/scripts/strategies/recursive-farming/req-withdraw.ts
+++ src/scripts/strategies/recursive-farming/req-withdraw.ts
@@ -1,6 +1,5 @@
 import { ethers } from "hardhat";
 import "@nomiclabs/hardhat-ethers";
-import { BigNumber } from "ethers";
 import { StrategyRecursiveFarming } from "../../../typechain";
 const logger = require("pino")();
 
@@ -9,10 +8,7 @@ const { CONTRACT_ADDRESS } = process.env;
 const GAS_LIMIT = 2074040;
 
 // function for user to request withdraw
-export async function reqWithdraw(
-  userAddr: string,
-  amount: BigNumber
-): Promise<void> {
+export async function reqWithdraw(userAddr: string): Promise<void> {
   // get strategy contract
   const strategyContract = await ethers.getContractFactory(
     "StrategyRecursiveFarming"
@@ -22,9 +18,13 @@ export async function reqWithdraw(
   ) as StrategyRecursiveFarming;
 
   try {
+    // get the max amount of quotas of the user from withdraw
+    const quotaQty = await strategy.quotasPerAddress();
+    logger.info(`The actual quota is ${quotaQty}`);
+
     // execute requestWithdraw from user address and with the amount to withdraw
     logger.info("Executing requestWithdraw...");
-    const reqTx = await strategy.requestWithdraw(amount, {
+    const reqTx = await strategy.requestWithdraw(quotaQty, {
       from: userAddr,
       gasLimit: GAS_LIMIT,
     });
