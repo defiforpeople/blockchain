2d41c1e0460ac662d93f54910dffad103b309186
NicoBarragan <nbarragan01@gmail.com>
2022-05-27 20:19:45 -0300

    feat(index.ts): added implementation for getting the quota price and the APY from the strategy contract

--- src/scripts/strategies/recursive-farming/index.ts
+++ src/scripts/strategies/recursive-farming/index.ts
@@ -6,21 +6,29 @@ import {
   // eslint-disable-next-line camelcase
   IERC20__factory,
   IERC20,
+  StrategyRecursiveFarming,
 } from "../../../typechain";
 import { getWeth } from "../../../utils/helpers/get-weth";
 import { deposit } from "./deposit";
-import { reqWithdraw } from "./withdraw";
+import { reqWithdraw } from "./req-withdraw";
 const logger = require("pino")();
 
 // defined constants
-const { WRAPPED_NATIVE_TOKEN_ADDRESS } = process.env;
-const AMOUNT = BigNumber.from(1000000000);
-const WITHDRAW_AMOUNT = BigNumber.from(100);
+const { WRAPPED_NATIVE_TOKEN_ADDRESS, CONTRACT_ADDRESS } = process.env;
+const AMOUNT = BigNumber.from("1000000000");
 
 // function for managing the execution flow of the other functions
 const main = async () => {
   // get the owner wallet
   const [wallet] = await ethers.getSigners();
+
+  // get the strategy contract
+  const strategyContract = await ethers.getContractFactory(
+    "StrategyRecursiveFarming"
+  );
+  const strategy = strategyContract.attach(
+    `${CONTRACT_ADDRESS}`
+  ) as StrategyRecursiveFarming;
 
   // get the ERC20 native token (WAVAX in this case)
   const token = (await ethers.getContractAt(
@@ -43,12 +51,18 @@ const main = async () => {
     );
   }
 
-  // logger.info("Doing Deposit...");
-  // await deposit(wallet.address, AMOUNT);
+  logger.info("Doing Deposit...");
+  await deposit(wallet.address, AMOUNT);
+
+  const quotaQty = await strategy.quotasPerAddress();
+  logger.info(`The actual quota is ${quotaQty}`);
 
   // execute withdraw from the address sender (in this case, the owner wallet)
   logger.info("Requesting Withdraw...");
-  await reqWithdraw(wallet.address, WITHDRAW_AMOUNT);
+  await reqWithdraw(wallet.address);
+
+  const apy = await strategy.getAPY();
+  logger.info(`The actual APY is ${apy}`);
 
   // get and print WAVAX balance after executing
   wavaxBalance = await token.balanceOf(wallet.address);
