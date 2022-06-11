f1e3ff71f6220cbdeeb02215527dc020ef78911b
NicoBarragan <nbarragan01@gmail.com>
2022-06-10 22:12:50 -0300

    feat(strategy-recursive-farming.test.ts) finished successfully beforeEach and tested constructor

--- src/contracts/IWETH.sol
+++ src/contracts/IWETH.sol
@@ -1,16 +1,10 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.8.10;
 
-interface IWETH {
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IWETH is IERC20 {
     function deposit() external payable;
 
     function withdraw(uint256) external;
-
-    function approve(address guy, uint256 wad) external returns (bool);
-
-    function transferFrom(
-        address src,
-        address dst,
-        uint256 wad
-    ) external returns (bool);
 }
