c6d34fd72a07faeb51c4ff5c77e66aabde629520
NicoBarragan <nbarragan01@gmail.com>
2022-06-15 11:59:12 -0300

    feat(strategy-recursive-farming.test.ts): now beforeEach functions with aave mock and token mock, tested to 2 differents tests in 'deposit' successfully

--- src/test/strategy-recursive-farming.test.ts
+++ src/test/strategy-recursive-farming.test.ts
@@ -2,20 +2,15 @@
 import {
   StrategyRecursiveFarming,
   // eslint-disable-next-line camelcase
-  IPool__factory,
-  // eslint-disable-next-line camelcase
-  IWETH__factory,
-  IWETH,
-  IERC20,
-  // eslint-disable-next-line camelcase
   StrategyRecursiveFarming__factory,
-  // eslint-disable-next-line camelcase
-  IERC20__factory,
+  MockV3Aggregator,
+  MockIncentivesController,
+  DFP,
+  MockPoolDFP,
 } from "../typechain";
 import { assert, expect, use } from "chai";
 import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
-import { deployMockContract, MockContract } from "ethereum-waffle";
 import { waffleChai } from "@ethereum-waffle/chai";
 import { BigNumber, logger, Signer } from "ethers";
 use(waffleChai);
@@ -26,12 +21,18 @@ describe("StrategyRecursiveFarming", () => {
   let userAddress: string;
   let ownerAddress: string;
 
-  let poolMockContract: MockContract;
-  let tokenMockContract: MockContract;
   let strategyContract: StrategyRecursiveFarming;
-  let wethToken: IWETH;
   let keeperInterval: BigNumber;
   let maxSupply: BigNumber;
+  let token: DFP;
+  let mockV3Aggregator: MockV3Aggregator;
+  let mockIncentivesCont: MockIncentivesController;
+  let gasPrice: BigNumber;
+  let poolMock: MockPoolDFP;
+
+  let userBalance: BigNumber;
+  let ownerBalance: BigNumber;
+  let poolBalance: BigNumber;
 
   enum StrategyStatus {
     Pristine,
@@ -42,13 +43,13 @@ describe("StrategyRecursiveFarming", () => {
 
   beforeEach(async () => {
     // prepare (signers) ownerWallet and userWallet
-    // send 1 eth from signer(0) to random ownerWallet and userWallet
     owner = ethers.provider.getSigner(0);
     ownerAddress = await owner.getAddress();
 
     user = ethers.provider.getSigner(2);
     userAddress = await user.getAddress();
 
+    // send 1 eth from signer(0) to random ownerWallet and userWallet
     await owner.sendTransaction({
       to: ownerAddress,
       value: ethers.utils.parseEther("1.0"),
@@ -58,63 +59,62 @@ describe("StrategyRecursiveFarming", () => {
       value: ethers.utils.parseEther("1.0"),
     });
 
-    // define AavePool mock contract
-    poolMockContract = await deployMockContract(owner, IPool__factory.abi);
+    // get gas price and set decimals, for MOCKVAgg contract, and for testing purpose
+    gasPrice = await ethers.provider.getGasPrice();
+    const decimals = BigNumber.from(18);
 
-    // define tokn mock contract
-    const WRAPPED_NATIVE_TOKEN_ADDRESS =
-      "0xd74047010D77c5901df5b0f9ca518aED56C85e8D"; // WETH address in rinkeb
-    tokenMockContract = await deployMockContract(owner, IERC20__factory.abi);
-    // tokenMockContract.mock.address.returns(WRAPPED_NATIVE_TOKEN_ADDRESS);
+    // get and deploy MockV3Aggregator contract (returns gas price)
+    const mockAggFactory = await ethers.getContractFactory("MockV3Aggregator");
+    mockV3Aggregator = (await mockAggFactory.deploy(
+      decimals,
+      gasPrice
+    )) as MockV3Aggregator;
 
-    // mock maxSupply for token
-    maxSupply = BigNumber.from("13798999");
-    tokenMockContract.mock.totalSupply.returns(maxSupply);
+    // get and deploy MockIncentivesController contract (for claiming funcitons)
+    const mockIncentivesFactory = await ethers.getContractFactory(
+      "MockIncentivesController"
+    );
+    mockIncentivesCont =
+      (await mockIncentivesFactory.deploy()) as MockIncentivesController;
 
-    // define other constants that the constructor requires for deploy
+    // // define AavePool mock contract
+    const poolFactory = await ethers.getContractFactory("MockPoolDFP");
+    poolMock = (await poolFactory.deploy()) as MockPoolDFP;
+
+    // define token mock contract (DFP token our own token for testing)
+    const dfpTokenFactory = await ethers.getContractFactory("DFP");
+    token = (await dfpTokenFactory.deploy()) as DFP;
+
+    // get wrapped native token to owner and user wallets
+    await token.mint(ownerAddress, ethers.utils.parseEther("1.0"));
+    await token.mint(userAddress, ethers.utils.parseEther("1.0"));
+    await token.mint(poolMock.address, ethers.utils.parseEther("10.0"));
+
+    // define keeperInterval for deploying the strategy contract
     keeperInterval = BigNumber.from(300); // define interval for keeper to execute
-    const GAS_DATA_FEED = ethers.constants.AddressZero;
-    const REWARDS_EMISSION_MANAGER = ethers.constants.AddressZero;
 
-    // deploy StrategyRecursiveFarming contract
+    // get and deploy StrategyRecursiveFarming contract
     const strategyRecursiveFarming = (await ethers.getContractFactory(
       "StrategyRecursiveFarming"
       // eslint-disable-next-line camelcase
     )) as StrategyRecursiveFarming__factory;
 
     strategyContract = await strategyRecursiveFarming.deploy(
-      poolMockContract.address,
-      `${GAS_DATA_FEED}`,
-      tokenMockContract.address,
-      `${REWARDS_EMISSION_MANAGER}`,
+      poolMock.address,
+      mockV3Aggregator.address,
+      token.address,
+      mockIncentivesCont.address,
       keeperInterval
     );
     await strategyContract.deployed();
 
-    // define the wrapped native token (WAVAX or WETH)
-    wethToken = (await ethers.getContractAt(
-      IWETH__factory.abi,
-      `${WRAPPED_NATIVE_TOKEN_ADDRESS}`,
-      user
-    )) as IWETH;
+    // get balances
+    userBalance = await token.balanceOf(userAddress);
+    ownerBalance = await token.balanceOf(ownerAddress);
+    poolBalance = await token.balanceOf(poolMock.address);
 
-    // get wrapped native token to owner and user wallets
-    await wethToken.deposit({
-      from: userAddress,
-      value: ethers.utils.parseEther("1.0"),
-    });
-
-    await wethToken.transfer(ownerAddress, ethers.utils.parseEther("0.5"), {
-      from: userAddress,
-    });
-
-    // fails, missing mocks of gas oracles and aave
-    // await strategyContract.deposit(ethers.utils.parseEther("0.1"), {
-    //   from: ownerAddress,
-    // });
-
-    // const userBalance = await wethToken.balanceOf(userAddress);
-    // logger.info(userBalance);
+    // get max supply
+    maxSupply = await token.totalSupply();
   });
 
   describe("constructor", () => {
@@ -122,22 +122,34 @@ describe("StrategyRecursiveFarming", () => {
       const status = await strategyContract.getStatus();
       const interval = await strategyContract.getInterval();
       const lastTimestamp = await strategyContract.getLastTimestamp();
-      const maxSupply = await strategyContract.getMaxSupply();
-
-      logger.info("Max Supply: ", maxSupply.toNumber());
-      logger.info("Last Timestamp: ", lastTimestamp.toNumber());
+      const maxSupplyContract = await strategyContract.getMaxSupply();
 
       expect(status).to.equal(StrategyStatus.Pristine);
       expect(interval).to.equal(keeperInterval);
-      expect(maxSupply).to.equal(maxSupply);
       assert(lastTimestamp > BigNumber.from(0));
+      expect(maxSupplyContract.toString()).to.equal(maxSupply.toString());
     });
   });
 
-  // it("tests deposit()", async () => {
-  //   const amount = ethers.utils.parseEther("0.3");
-  //   await poolMockContract.mock.supply.returns(
-  //     token.transferFrom(userWallet.address, poolMockContract.address, amount)
-  //   );
-  // });
+  describe("deposit", () => {
+    it("tests revert with enough balance error", async () => {
+      const amount = ethers.utils.parseEther("111");
+      await expect(
+        strategyContract.deposit(amount, { from: ownerAddress })
+      ).to.be.revertedWith("Error__NotEnoughBalance()");
+    });
+
+    it("tests deposit makes 'transferFrom' successfully", async () => {
+      const amount = ethers.utils.parseEther("0.1");
+      const firstBalance = await token.balanceOf(ownerAddress);
+
+      await token.approve(strategyContract.address, amount, {
+        from: ownerAddress,
+      });
+      await strategyContract.deposit(amount, { from: ownerAddress });
+      const secondBalance = await token.balanceOf(ownerAddress);
+
+      expect(firstBalance.sub(amount)).to.equal(secondBalance);
+    });
+  });
 });
