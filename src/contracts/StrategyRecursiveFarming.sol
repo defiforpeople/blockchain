067fcde096c2bf128c3e8a41955c635b342e685b
NicoBarragan <nbarragan01@gmail.com>
2022-06-17 21:04:23 -0300

    feat(StrategyRecursiveFarming.sol): deleted 'doRecursion' function, updated 'checkUpkeep', 'performUpkeep' '_supply' and '_borrow' functions. added 'checkAmount' modifier

--- src/contracts/StrategyRecursiveFarming.sol
+++ src/contracts/StrategyRecursiveFarming.sol
@@ -17,8 +17,13 @@ import "hardhat/console.sol";
 // errors
 error Error__NotEnoughBalance();
 error Error__NotEnoughQuotas();
-error Error__UpkeepNotNeeded(uint256 raffleState);
+error Error__UpkeepNotNeeded(
+    uint256 raffleState,
+    uint256 timePassed,
+    uint256 interval
+);
 error Error__RecursionNotNeeded(uint256 raffleState);
+error Error__AmountIsZero(uint256 amount);
 
 contract StrategyRecursiveFarming is
     Pausable,
@@ -35,28 +40,24 @@ contract StrategyRecursiveFarming is
     // internal
     uint256 private investmentAmount;
     StrategyStatus private status;
-    uint256 private lastTimestamp;
-    uint256 private interval;
+    uint256 private _lastTimestamp;
+    uint256 private _interval;
     uint256 public totalInvested;
     address[] public tokenAddresses;
-    uint256 private immutable wavaxTotalSupply;
+    uint256 private immutable _wavaxTotalSupply;
+    uint256 private _quotas;
+    uint256 private _gasPriceMultiplier; // multiplier of gas price in if conditionals
+    uint16 private _aaveRefCode;
 
     // constants
     uint256 private constant GAS_USED_DEPOSIT = 345304;
     uint256 private constant GAS_USED_BORROW = 321838;
     uint256 private constant GAS_USED_SUPPLY = 250410;
-    uint256 private constant GAS_PRICE_MULTIPLIER = 0; // multiply for 0 for using less amount in a testnet version ;)
     uint256 private constant INTEREST_RATE_MODE = 2; // the borrow is always in variable mode
-    uint16 private constant AAVE_REF_CODE = 0;
     uint256 private constant BASE_QUOTA = 1;
 
-    struct Invest {
-        uint256 amount;
-        uint256 quotas;
-    }
-
     // define investments information
-    mapping(address => Invest) public _investments;
+    mapping(address => uint256) private _investments;
 
     // status that marks the next step to do in the strategy
     enum StrategyStatus {
@@ -75,17 +76,19 @@ contract StrategyRecursiveFarming is
         address _gasPriceFeedAddr,
         address _wavaxAddr,
         address _aaveRewardsManager,
-        uint256 _interval
+        uint256 interval,
+        uint256 gasPriceMultiplier
     ) {
         aavePool = IPool(_aavePoolAddr);
         gasPriceFeed = AggregatorV3Interface(_gasPriceFeedAddr);
         token = IERC20(_wavaxAddr);
         rewardsManager = IRewardsController(_aaveRewardsManager); // for claiming rewards
         status = StrategyStatus.Pristine;
-        interval = _interval; // for keeper logic
-        lastTimestamp = block.timestamp; // for keeper logic
+        _interval = interval; // for keeper logic
+        _lastTimestamp = block.timestamp; // for keeper logic
         tokenAddresses.push(_wavaxAddr); // token for claiming rewards
-        wavaxTotalSupply = token.totalSupply(); // for getQuotaPrice logic
+        _wavaxTotalSupply = token.totalSupply(); // for getQuotaPrice logic
+        _gasPriceMultiplier = gasPriceMultiplier;
     }
 
     // method defined for the user to make an supply, and we save the investment amount with his address
@@ -94,64 +97,45 @@ contract StrategyRecursiveFarming is
         if (token.balanceOf(address(msg.sender)) < _amount) {
             revert Error__NotEnoughBalance();
         }
-        console.log("Not reverted");
+
         // transfer the user amount to this contract (user has to approve before this)
         token.transferFrom(msg.sender, address(this), _amount);
-        console.log("Transferred from");
 
-        // save amount in the mapping
-        _investments[msg.sender].amount += _amount;
         // calculate and save quotas
-        console.log("Update investments");
-        uint256 quotas = _getQuotaQty(_amount);
-        console.log("Get quotas");
-        _investments[msg.sender].quotas += quotas;
-        console.log("Update quotas");
+        _quotas = _getQuotaQty(_amount);
+        _investments[msg.sender] += _quotas;
         // sum amount to totalInvested
         totalInvested += _amount;
-        console.log("Update amount");
 
         // approve and supply liquidity to the protocol
         token.approve(address(aavePool), _amount);
-        console.log("token approved");
-        aavePool.supply(address(token), _amount, address(this), AAVE_REF_CODE);
-        console.log("liquidity supplied");
+        aavePool.supply(address(token), _amount, address(this), _aaveRefCode);
 
         // update the status of the strategy to the next step to do
         status = StrategyStatus.Borrow;
 
         // emit Deposit event
-        emit Deposit(msg.sender, _amount, quotas);
-        console.log("All finished");
+        emit Deposit(msg.sender, _amount, _quotas);
     }
 
-    function _borrow() internal {
-        // get the gas price
-        (, int256 gasPrice, , , ) = gasPriceFeed.latestRoundData();
-
-        // get the max available amount ofr borrowing
-        (, , uint256 borrowAvailable, , , ) = aavePool.getUserAccountData(
-            address(this)
-        );
-
-        // if the amount is not enough for continuing with the execution, the status will be Done and the exec'll be finished
-        if (
-            borrowAvailable <
-            (GAS_USED_DEPOSIT + GAS_USED_SUPPLY) *
-                uint256(gasPrice) *
-                GAS_PRICE_MULTIPLIER
-        ) {
-            status = StrategyStatus.Done;
-            return;
+    // Modifier for asserting that amount is greater than 0
+    modifier checkAmount(uint256 _amount) {
+        if (_amount == 0) {
+            revert Error__AmountIsZero(_amount);
         }
-        // otherwise, it will continue with the execution flow
+        _;
+    }
 
+    function _borrow(uint256 _borrowAvailable)
+        internal
+        checkAmount(_borrowAvailable)
+    {
         // method for borrow in Aave
         aavePool.borrow(
             address(token),
-            borrowAvailable,
+            _borrowAvailable,
             INTEREST_RATE_MODE,
-            AAVE_REF_CODE,
+            _aaveRefCode,
             address(this)
         );
 
@@ -160,82 +144,117 @@ contract StrategyRecursiveFarming is
     }
 
     // method for supply liquidity to aave
-    function _supply() internal {
-        // get the gas price
-        (, int256 gasPrice, , , ) = gasPriceFeed.latestRoundData();
-
-        // if the amount is not enough for continuing with the execution, the status will be Done and the exec'll be finished
-        if (
-            token.balanceOf(address(this)) <
-            GAS_USED_DEPOSIT * uint256(gasPrice) * GAS_PRICE_MULTIPLIER
-        ) {
-            status = StrategyStatus.Done;
-            return;
-        } // otherwise, it will continue with the execution flow:
-
-        // the var amount will contain the WAVAX of the contract(what we borrowed before)
-        uint256 amount = token.balanceOf(address(this));
-
+    function _supply(uint256 _totalBalance)
+        internal
+        checkAmount(_totalBalance)
+    {
         // approve and supply liquidity
-        token.approve(address(aavePool), amount);
-        aavePool.supply(address(token), amount, address(this), AAVE_REF_CODE);
-
-        // get the max available amount ofr borrowing
-        (, , uint256 borrowAvailable, , , ) = aavePool.getUserAccountData(
-            address(this)
+        token.approve(address(aavePool), _totalBalance);
+        aavePool.supply(
+            address(token),
+            _totalBalance,
+            address(this),
+            _aaveRefCode
         );
 
-        // if the amount is not enough for continuing with the execution, the status will be Done and the exec will be finished
-        if (
-            borrowAvailable <
-            (GAS_USED_DEPOSIT + GAS_USED_SUPPLY) *
-                uint256(gasPrice) *
-                GAS_PRICE_MULTIPLIER
-        ) {
-            // update status to done, because the loop has finished
-            status = StrategyStatus.Done;
-            return;
-        }
-
-        // otherwise, update status to borrow, that is the next step
+        // update status to borrow, that is the next step
         status = StrategyStatus.Borrow;
     }
 
-    // method for executing the loop, based on the status of the contract
-    function doRecursion() external onlyOwner {
-        if (
-            status == StrategyStatus.Done || status == StrategyStatus.Pristine
-        ) {
-            revert Error__RecursionNotNeeded(uint256(status));
+    // method that uses keeper for know if it has to executo performUpkeep() or not
+    function checkUpkeep(
+        bytes memory /* checkData */
+    )
+        public
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        bool intervalPassed = (block.timestamp - _lastTimestamp) > _interval;
+        bool statusIsOk = status == StrategyStatus.Borrow ||
+            status == StrategyStatus.Supply;
+        upkeepNeeded = intervalPassed && statusIsOk;
+    }
+
+    // method for executing the recursive loop based on the status of the strategy with the keeper
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Error__UpkeepNotNeeded(
+                uint256(status),
+                block.timestamp - _lastTimestamp,
+                _interval
+            );
         }
 
+        // get the gas price
+        (, int256 gasPrice, , , ) = gasPriceFeed.latestRoundData();
+
         if (status == StrategyStatus.Borrow) {
-            _borrow();
+            // get the max available amount for borrowing
+            (, , uint256 borrowAvailable, , , ) = aavePool.getUserAccountData(
+                address(this)
+            );
+
+            // if the amount is not enough for continuing with the execution, the status will be Done and the exec will be finished
+            if (
+                borrowAvailable <=
+                (GAS_USED_BORROW + GAS_USED_SUPPLY) *
+                    uint256(gasPrice) *
+                    _gasPriceMultiplier
+            ) {
+                // update status to done, because the loop has finished
+                status = StrategyStatus.Done;
+                _lastTimestamp = block.timestamp;
+                return;
+            }
+
+            // otherwise, it will execute _borrow() function
+            _borrow(borrowAvailable);
         } else if (status == StrategyStatus.Supply) {
-            _supply();
+            // if the amount is not enough for continuing with the execution, the status will be Done and the exec'll be finished
+            console.log("gas");
+            console.logUint(uint256(gasPrice));
+            if (
+                token.balanceOf(address(this)) <
+                GAS_USED_SUPPLY * uint256(gasPrice) * _gasPriceMultiplier
+            ) {
+                status = StrategyStatus.Done;
+                _lastTimestamp = block.timestamp;
+                return;
+            }
+
+            // otherwise, it will execute _supply() function
+            uint256 totalBalance = token.balanceOf(address(this));
+            _supply(totalBalance);
         }
+        // update the last time that the keeper executed the function
+        _lastTimestamp = block.timestamp;
     }
 
     // method defined for the user can withdraw from the strategy
-    function requestWithdraw(uint256 _quotas) external {
+    function requestWithdraw(uint256 quotas) external {
         // check if user has requested amount
         if (
-            _investments[msg.sender].quotas > 0 &&
-            _investments[msg.sender].quotas >= _quotas
+            _investments[msg.sender] > 0 && _investments[msg.sender] >= quotas
         ) {
             revert Error__NotEnoughQuotas();
         }
         // rest the amount repayed of investments
-        uint256 amount = _quotas * _getQuotaPrice();
-        // uint256 quotas = _amount * _getQuotaPrice();
-        _investments[msg.sender].amount -= amount;
-        _investments[msg.sender].quotas -= _quotas;
+        uint256 amount = quotas * _getQuotaPrice();
+        // uint256 _quotas = _amount * _getQuotaPrice();
+        _investments[msg.sender] -= quotas;
 
         // repay the Aave loan with collateral
         aavePool.repayWithATokens(address(token), amount, INTEREST_RATE_MODE);
 
         // update the status of strategy for the next step needed
-        emit Withdraw(msg.sender, amount, _quotas);
+        emit Withdraw(msg.sender, amount, quotas);
     }
 
     // method for withdraw and transfer tokens to the users
@@ -272,49 +291,32 @@ contract StrategyRecursiveFarming is
             totalInvested;
 
         // normalized profit + BASE_QUOTA is for avoid quotaPrice to be 0
-        uint256 quotaPrice = BASE_QUOTA + ((profit * 100) / wavaxTotalSupply);
+        uint256 quotaPrice = BASE_QUOTA + ((profit * 100) / _wavaxTotalSupply);
         return quotaPrice;
     }
 
-    // method that uses keeper for know if it has to executo performUpkeep() or not
-    function checkUpkeep(
-        bytes memory /* checkData */
-    )
-        public
-        view
-        override
-        returns (
-            bool upkeepNeeded,
-            bytes memory /* performData */
-        )
+    // method for update the _gasPriceMultiplier externally
+    function setGasPriceMultiplier(uint256 gasPriceMultiplier)
+        external
+        onlyOwner
     {
-        upkeepNeeded =
-            (block.timestamp - lastTimestamp) > interval &&
-            status != StrategyStatus.Done;
+        if (_gasPriceMultiplier == gasPriceMultiplier) {
+            return;
+        }
+        _gasPriceMultiplier = gasPriceMultiplier;
     }
 
-    // method for executing the recursive loop based on the status of the strategy with the keeper
-    function performUpkeep(
-        bytes calldata /* performData */
-    ) external override {
-        (bool upkeepNeeded, ) = checkUpkeep("");
-        if (!upkeepNeeded) {
-            revert Error__UpkeepNotNeeded(uint256(status));
+    // method for update aave Referral code if necessary :)
+    function setAaveRefCode(uint16 aaveRefCode) external onlyOwner {
+        if (_aaveRefCode == aaveRefCode) {
+            return;
         }
-        require(status != StrategyStatus.Done, "The strategy is completed");
-
-        if (status == StrategyStatus.Borrow) {
-            _borrow();
-        } else if (status == StrategyStatus.Supply) {
-            _supply();
-        }
-        // update the last time that the keeper executed the function
-        lastTimestamp = block.timestamp;
+        _aaveRefCode = aaveRefCode;
     }
 
-    // method for updating keeper interval
-    function updateInterval(uint256 _interval) external onlyOwner {
-        interval = _interval;
+    // method for updating keeper _interval
+    function updateInterval(uint256 interval) external onlyOwner {
+        _interval = interval;
     }
 
     /* View functions */
@@ -338,9 +340,17 @@ contract StrategyRecursiveFarming is
         return aavePool.getReserveNormalizedIncome(address(this));
     }
 
-    // amount of quotas per address
-    function quotasPerAddress() external view returns (uint256) {
-        return _investments[msg.sender].quotas;
+    // amount of _quotas per address
+    function getQuotasPerAddress(address _investor)
+        external
+        view
+        returns (uint256)
+    {
+        return _investments[_investor];
+    }
+
+    function getTotalInvested() external view returns (uint256) {
+        return totalInvested;
     }
 
     // this method returns the status of the strategy
@@ -348,25 +358,39 @@ contract StrategyRecursiveFarming is
         return status;
     }
 
+    // method for get the _gasPriceMultiplier externally
+    function getGasPriceMultiplier() external view returns (uint256) {
+        return _gasPriceMultiplier;
+    }
+
     function getLastTimestamp() external view onlyOwner returns (uint256) {
-        return lastTimestamp;
+        return _lastTimestamp;
     }
 
     function getInterval() external view onlyOwner returns (uint256) {
-        return interval;
+        return _interval;
     }
 
     function getMaxSupply() external view returns (uint256) {
-        return wavaxTotalSupply;
+        return _wavaxTotalSupply;
     }
 
-    function getGasUsed()
+    // method for getting gas info (for testing purpose)
+    function getGasInfo()
         external
         view
         returns (
-            uint256 GAS_USED_DEPOSIT,
-            uint256 GAS_USED_SUPPLY,
-            uint256 GAS_USED_BORROW
+            uint256,
+            uint256,
+            uint256,
+            uint256
         )
-    {}
+    {
+        return (
+            GAS_USED_DEPOSIT,
+            GAS_USED_SUPPLY,
+            GAS_USED_BORROW,
+            _gasPriceMultiplier
+        );
+    }
 }
