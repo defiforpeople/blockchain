a25b6e41d67ec14a70154e6cf0f96e1ede4e5df1
NicoBarragan <nbarragan01@gmail.com>
2022-07-13 16:01:16 -0300

    feat(IStrategy.sol): added events 'Deposit' and 'Withdraw' in the interface

--- src/contracts/IStrategy.sol
+++ src/contracts/IStrategy.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.0;
 
 interface IStrategy {
+    event Deposit(address indexed userAddr, uint256 amount, uint256 quotas);
+
+    event Withdraw(address indexed userAddr, uint256 amount, uint256 quotas);
+
     function getQuotaQty(uint256 amount) external view returns (uint256);
 
     function getQuotaPrice() external view returns (uint256);
