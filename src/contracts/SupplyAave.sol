c7a97107acdf948f5a507abf3a5605079a932a4b
NicoBarragan <nbarragan01@gmail.com>
2022-07-15 19:29:13 -0300

    feat(SupplyAave.sol): created first version of SupplyAave strategy, using supplyWithPermit() and withdraw() Aave methods for executing the functions

--- src/contracts/SupplyAave.sol
+++ src/contracts/SupplyAave.sol
@@ -2,40 +2,31 @@
 
 pragma solidity ^0.8.10;
 
-import {IStrategy} from "./IStrategy.sol";
 import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 // errors
 error Error__NotEnoughBalance(uint256 balance);
-error Error__NotEnoughQuotas(uint256 quotas, uint256 requestedQuotas);
+error Error__NotEnoughAllowance(uint256 allowance);
+error Error__NotEnoughLP(uint256 lpAmount);
 error Error__AmountIsZero();
+error Error__InvalidToken();
 
-contract SupplyAave is IStrategy, Ownable {
-    using SafeMath for uint256;
+contract SupplyAave {
+    // events
+    event Deposit(address indexed userAddr, uint256 amount);
+    event Withdraw(address indexed userAddr, uint256 lpAmount);
 
-    IERC20 public immutable token;
-    IPool public immutable aavePool;
-    AggregatorV3Interface public immutable gasPriceFeed;
-    uint16 public referralCode = 0;
+    // variables
+    IPool internal immutable _aavePool;
+    uint16 internal immutable _aaveRefCode;
 
-    // define investments information
-    mapping(address => uint256) private _investments;
-
-    // TODO: add implementation for using multiple ERC20 tokens
-    constructor(
-        address _token,
-        address _aavePool,
-        address _gasPriceFeed
-    ) {
-        token = IERC20(_token);
-        aavePool = IPool(_aavePool);
-        gasPriceFeed = AggregatorV3Interface(_gasPriceFeed);
+    constructor(address aavePool, uint16 aaveRefCode) {
+        _aavePool = IPool(aavePool);
+        _aaveRefCode = aaveRefCode;
     }
 
+    // modifier that checks amount is not zero
     modifier checkAmount(uint256 amount) {
         if (amount == 0) {
             revert Error__AmountIsZero();
@@ -43,57 +34,72 @@ contract SupplyAave is IStrategy, Ownable {
         _;
     }
 
-    function supply(uint256 _amount) external checkAmount(_amount) {
-        if (token.balanceOf(msg.sender) < _amount || _amount == 0) {
+    // TODO(nb): check if the token is available in Aave pool
+    // modifier that checks if the token is available in Aave pool
+    modifier checkToken(address token) {
+        if (token == address(0)) {
+            revert Error__InvalidToken();
+        }
+        _;
+    }
+
+    // method for supplying tokens to Aave pool, with user permittion
+    function supply(
+        uint256 amount,
+        address tokenAddr,
+        uint256 deadline,
+        uint8 permitV,
+        bytes32 permitR,
+        bytes32 permitS
+    ) external checkAmount(amount) checkToken(tokenAddr) {
+        IERC20 token = IERC20(tokenAddr);
+
+        // check that user give enoguh allowance to the contract for supplying the token
+        if (
+            token.allowance(msg.sender, address(this)) == 0 ||
+            token.allowance(msg.sender, address(this)) < amount
+        ) {
+            revert Error__NotEnoughAllowance(
+                token.allowance(msg.sender, address(this))
+            );
+        }
+
+        // check that user has enough balance to supply the token
+        if (token.balanceOf(msg.sender) < amount || amount == 0) {
             revert Error__NotEnoughBalance(token.balanceOf(msg.sender));
         }
 
-        token.transferFrom(msg.sender, address(this), _amount);
+        // supply tokens to Aave pool
+        _aavePool.supplyWithPermit(
+            (address(token)),
+            amount,
+            address(this),
+            _aaveRefCode,
+            deadline,
+            permitV,
+            permitR,
+            permitS
+        );
 
-        aavePool.supply((address(token)), _amount, address(this), referralCode);
-
-        uint256 quotas = _getQuotaQty(_amount);
-
-        emit Deposit(msg.sender, _amount, quotas);
+        emit Deposit(msg.sender, amount);
     }
 
-    function withdraw(uint256 _quotasAmm) external checkAmount(_quotasAmm) {
-        uint256 userQuotas = _investments[msg.sender];
-        if (userQuotas == 0 || _quotasAmm > userQuotas) {
-            revert Error__NotEnoughQuotas(userQuotas, _quotasAmm);
+    // method for withdraw tokens from Aave pool, to user's address
+    function withdraw(uint256 lpAmount, address tokenAddr)
+        external
+        checkAmount(lpAmount)
+        checkToken(tokenAddr)
+    {
+        IERC20 token = IERC20(tokenAddr);
+
+        // check that user has enough LP to withdraw
+        if (token.balanceOf(msg.sender) < lpAmount || lpAmount == 0) {
+            revert Error__NotEnoughBalance(token.balanceOf(msg.sender));
         }
 
-        uint256 amount = _getAmountFromQuotas(_quotasAmm);
-        // withdraw token amount from aave, to the userAddr
-        aavePool.withdraw(address(token), amount, msg.sender);
-        emit Withdraw(msg.sender, amount, _quotasAmm);
-    }
+        // withdraw token lpAmount from aave, to the userAddr
+        _aavePool.withdraw(address(token), lpAmount, msg.sender);
 
-    function getQuotaQty(uint256 amount) external view returns (uint256) {
-        return _getQuotaQty(amount);
-    }
-
-    function _getQuotaQty(uint256 amount) internal view returns (uint256) {
-        return amount / 2;
-    }
-
-    function getAmountFromQuotas(uint256 quotas)
-        external
-        view
-        returns (uint256)
-    {
-        return _getAmountFromQuotas(quotas);
-    }
-
-    function _getAmountFromQuotas(uint256 quotas)
-        internal
-        view
-        returns (uint256)
-    {
-        return quotas * 2;
-    }
-
-    function getQuotaPrice() external view returns (uint256) {
-        return 1;
+        emit Withdraw(msg.sender, lpAmount);
     }
 }
