578e7c427ae420337d379df07b7d9ad5648fff8d
NicoBarragan <nbarragan01@gmail.com>
2022-07-17 15:13:54 -0300

    refactor(withdraw.ts): added if assertion for .env variable and updated 'userAddr' param'

--- src/scripts/aave/withdraw.ts
+++ src/scripts/aave/withdraw.ts
@@ -3,19 +3,22 @@ import { BigNumber } from "ethers";
 import { SupplyAave, SupplyAave__factory } from "../../typechain";
 
 const { SUPPLY_CONTRACT_ADDRESS } = process.env;
+if (!SUPPLY_CONTRACT_ADDRESS || SUPPLY_CONTRACT_ADDRESS === "") {
+  throw new Error("invalid ENV values");
+}
 
 export async function withdraw(
   lpAmount: BigNumber,
   tokenAddr: string,
-  senderAddr: string
+  userAddr: string
 ) {
   const supplyContract = (await ethers.getContractAt(
     SupplyAave__factory.abi,
-    `${SUPPLY_CONTRACT_ADDRESS}`
+    SUPPLY_CONTRACT_ADDRESS!
   )) as SupplyAave;
 
   const withdrawTx = await supplyContract.withdraw(lpAmount, tokenAddr, {
-    from: senderAddr,
+    from: userAddr,
   });
   await withdrawTx.wait();
 }
