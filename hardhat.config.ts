9153b689bacf18bd633a7a025ab58a9ed44eb446
NicoBarragan <nbarragan01@gmail.com>
2022-07-17 15:35:18 -0300

    feat(hardhat.config.ts): added mumbai and polygon as networks params

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -232,6 +232,16 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    mumbai: {
+      url: process.env.URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    polygon: {
+      url: process.env.URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
